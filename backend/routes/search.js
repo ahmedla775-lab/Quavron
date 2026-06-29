239eaa53096d1df3e848c68db60b4490d2645f44
sponsnordj <nwraldynbnywsf67@gmail.com>
2026-06-29 22:42:40 +0100

    Update search.js

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -1,1 +1,20 @@
+const express = require("express");
 
+const router = express.Router();
+
+/* ========================================
+Search
+======================================== */
+
+router.get("/", (req, res) => {
+
+const query = req.query.q;
+
+res.json({
+success: true,
+query
+});
+
+});
+
+module.exports = router;
